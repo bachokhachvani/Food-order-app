e5d423052d1705b1a879ee26de89c2ddbd147e81
bachokhachvani <khachvanis@gmail.com>
2022-03-26 18:30:57 +0400

    first commit

---
+++ food-app/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+
+function HeaderCartButton(props) {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const numberOfCartItems = cartCtx.items.reduce((cartNumber, item) => {
+    return cartNumber + item.amount;
+  }, 0);
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon></CartIcon>
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+}
+
+export default HeaderCartButton;
